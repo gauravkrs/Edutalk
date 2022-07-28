511d6bc5fe07892c991ae20190b13817f02c1081
ShreyanshGuptaWD <shreyanshgupta148@gmail.com>
2022-07-28 10:01:16 +0530

    update

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -8,13 +8,10 @@ import {
   Text,
   VisuallyHidden,
   Image,
-  Input,
-  IconButton,
   useColorModeValue,
 } from "@chakra-ui/react";
 import IMAGE from "../Navbar/Edutalk.png";
 import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
-import { BiMailSend } from "react-icons/bi";
 
 const Logo = (props) => {
   return (
